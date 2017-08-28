79f4ebca39b1a41e8eaccd0da0184d29311fffbb
T0day <zhang_juntao@sjtu.edu.cn>
2017-08-28 15:11:58 +0800

    Add the copyright information

--- visualization/relationship_d3.js
+++ visualization/relationship_d3.js
@@ -1,3 +1,36 @@
+/*Copyright (c) 2013-2016, Rob Schmuecker
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* The name Rob Schmuecker may not be used to endorse or promote products
+  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL MICHAEL BOSTOCK BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Author: Rob Schmuecker <robert.schmuecker@gmail.com>
+Link: http://bl.ocks.org/robschmuecker/7880033 (D3.js Drag and Drop, Zoomable, Panning, Collapsible Tree with auto-sizing.)
+Reference code: Line34- Line127, Line394- Line553
+*/
+
+
 treeJSON = d3.json("example_data.json", function(error, treeData) {
     var totalNodes = 0;
     var maxLabelLength = 0;
@@ -517,8 +550,6 @@ treeJSON = d3.json("example_data.json", function(error, treeData) {
             d.x0 = d.x;
             d.y0 = d.y;
         });
-
-
 
         node.on("mouseover",function(d){
             if (!(d.children || d._children)){
